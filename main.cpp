9544b94ff2f3ea2368a67e9716c44a83ca758511
Jazerzxc <james992927108@gmail.com>
2017-10-06 00:47:09 +0800

    version 2

--- main.cpp
+++ main.cpp
@@ -11,103 +11,150 @@ const int dimension = 2;
 const int totalNodes = 50; // 城市数量
 int nodes[totalNodes][dimension+2];
 
-const int T0 = 1000;// 初始温度
-const int T_END = 0;
-const double q = 0.98;  // 退火系数
-const int L = 100;  // 每个温度时的迭代次数，即链长
+double T = 10000;// 初始温度
+double T_END = 0.00001;
+double q = 0.999;  // 退火系数
 
+
+const double r = 0.9;
 
 void ReadFile();
 void create_new();
-double path_len(int row);  // 计算路径长度
-double distance(int node1,int node2);
+int path_len(int row);
+int distance(int node1,int node2);
+void TwoOptSwap(int i, int k);
+
 int main(){
-    srand((unsigned)time(NULL)); //初始化随机数种子
+    srand((unsigned)time(NULL));
     time_t start,finish;
-    start = clock(); // 程序运行开始计时
-    double T;
-    int count = 0; // 记录降温次数
-    T = T0; //初始温度
+    int count = 0;
     ReadFile();
-    cout << "start simualate" << endl;
-    double f1,f2,df; //f1为初始解目标函数值，f2为新解目标函数值，df为二者差值
-    double r = 0.8; // 0-1之间的随机数，用来决定是否接受新解
+    start = clock();
+    int f1,f2,df;
     while(T > T_END){
-        for(int i = 0;i < L ;i++) {
+        for(int j = 0 ;j < totalNodes ;j++) {
+            nodes[j][3] = nodes[j][0];
+        }
+        f1 = path_len(3);//nodes[][3]
+        create_new();
+        f2 = path_len(0);//nodes[][0]
+        df = f2 - f1;
+        /*if(df < 0) {
             for(int j = 0 ;j < totalNodes ;j++) {
                 nodes[j][3] = nodes[j][0];
             }
-            create_new();
-            f1 = path_len(3);//nodes[][3]
-            f2 = path_len(0);//nodes[][0]
-            df = f2 - f1;
-            // 以下是Metropolis准则
-            if(df >= 0)
+        }else{
+            if(exp(-df/T) >= r){
+                for(int j = 0 ;j < totalNodes ;j++) {
+                    nodes[j][3] = nodes[j][0];
+                }
+            }else{
+                for(int j = 0 ;j < totalNodes ;j++) {
+                    nodes[j][0] = nodes[j][3];
+                }
+            }
+        }*/
+        if(df >= 0)
+        {
+            if(exp(-df/T) <= r) // 保留原来的解
             {
-                if(exp(-df/T) <= r) // 保留原来的解
-                {
-                    for(int j = 0 ;j < totalNodes ;j++) {
-                        nodes[j][3] = nodes[j][0];
-                    }
+                for(int j = 0 ;j < totalNodes ;j++) {
+                    nodes[j][0] = nodes[j][3];
                 }
             }
         }
-        T *= q; // 降温
+        //printf("%d\n",count);
+        T *= q;
         count++;
     }
     finish = clock(); // 退火过程结束
 
     double duration = ((double)(finish-start))/CLOCKS_PER_SEC; // 计算时间
-    printf("采用模拟退火算法，初始温度T0=%.2d,降温系数q=%.2f,每个温度迭代%d次,共降温%d次，得到的TSP最优路径为:\n",T0,q,L,count);
-    for(int i=0;i<totalNodes;i++)  // 输出最优路径
+    /*for(int i=0;i<totalNodes;i++)
     {
         printf("%d_%d\n",i+1,nodes[i][0]);
-    }
-    printf("%d\n",nodes[totalNodes-1]);
-    double len = path_len(0); // 最优路径长度
-    printf("最优路径长度为:%lf\n",len);
-    printf("程序运行耗时:%lf秒.\n",duration);
+    }*/
+    int len = path_len(0);
+    printf("count:%ld\n",count);
+    printf("path length:%ld\n",len);
+    printf("time :%lfs.\n",duration);
     return 0;
 }
 
-double distance(int node1,int node2)
+int distance(int node1,int node2)
 {
     double x1 = nodes[node1][1];
     double y1 = nodes[node1][2];
     double x2 = nodes[node2][1];
     double y2 = nodes[node2][2];
-    double dis = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-    //printf("dis__%d\n",dis);
+    int dis = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
     return dis;
 }
-// 计算路径长度
-double path_len(int row)
+
+int path_len(int row)
 {
-    int path = 0; // 初始化路径长度
+    int path = 0;
     for(int i= 0 ;i < totalNodes; i++){
         int index1 = nodes[i][row];
         int index2 = nodes[i+1][row];
-        //printf("%d\t%d\n",index1,index2);
         int dis = distance(index1,index2);
         path += dis;
     }
-    //printf("path__%d\n",path);
-    return path; // 返回总的路径长度
+    path += distance(nodes[0][row],nodes[totalNodes][row]);
+    return path;
 }
 
-// 产生一个新解
-// 此处采用随机交叉两个位置的方式产生新的解
+void TwoOptSwap(int i, int k ) {
+    // 1. take route[0] to route[i-1] and add them in order to new_route
+    for ( int c = 0; c <= i - 1; c++ ) {
+        nodes[c][3] = nodes[c][0];
+    }
+    // 2. take route[i] to route[k] and add them in reverse order to new_route
+    int dec = 0;
+    for ( int c = i; c <= k; c++ ) {
+        nodes[c][3] = nodes[k-dec][0];
+        dec++;
+    }
+    // 3. take route[k+1] to end and add them in order to new_route
+    for ( int c = k + 1; c < totalNodes; c++ ) {
+        nodes[c][3] = nodes[c][0];
+    }
+    for(int i= 0 ;i < totalNodes ; i++)
+    {
+        nodes[i][0] = nodes[i][3];
+    }
+}
 void create_new()
 {
+    /*int pos[10];
+    for(int i = 0; i < 10;i++)
+    {
+        double r = ((double)rand())/(RAND_MAX+1.0);
+        pos[i] = (int)(totalNodes*r);
+    }
+    for(int i = 0; i < 9 ;i++) {
+        int temp = nodes[pos[i]][0];
+        nodes[pos[i]][0] = nodes[pos[i + 1]][0];
+        nodes[pos[i + 1]][0] = temp;
+    }*/
     double r1 = ((double)rand())/(RAND_MAX+1.0);
-    double r2 = ((double)rand())/(RAND_MAX+1.0);
-    int pos1 = (int)(totalNodes*r1); //第一个交叉点的位置
-    int pos2 = (int)(totalNodes*r2);
-    int temp = nodes[pos1][0];
-    nodes[pos1][0] = nodes[pos2][0];
-    nodes[pos2][0] = temp;   // 交换两个点
+    int i = (int)(totalNodes * r1);
+    int k = 0;
+    while(k < i)
+    {
+        double r2 = ((double)rand())/(RAND_MAX+1.0);
+        k = (int)(totalNodes * r2);
+    }
+    printf("r1 = %d r2 = %d\n",i,k);
+    for(int i= 0 ;i < totalNodes ; i++) {
+        printf("%i--%d\n",i,nodes[i][0]);
+    }
+    TwoOptSwap( i, k );
+    printf("after\n");
+    for(int i= 0 ;i < totalNodes ; i++) {
+        printf("%i--%d\n",i,nodes[i][0]);
+    }
 }
-
 
 void ReadFile() {
     fstream file;
