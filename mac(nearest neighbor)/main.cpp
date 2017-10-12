4b54999d8489d63861a195fa07519d09fd35e18b
Jazerzxc <james992927108@gmail.com>
2017-10-12 08:51:24 +0800

    finish(demo)

--- mac(nearest neighbor)/main.cpp
+++ mac(nearest neighbor)/main.cpp
@@ -4,175 +4,184 @@
 #include <cmath>
 #include <thread>
 #include <cstring>
+#include <unistd.h>
 
 using namespace std;
 
 const int dimension = 2;
 const int totalNodes = 50; // 城市数量
 int nodes[totalNodes][dimension+4];
+int table[totalNodes][totalNodes];
+void reafFile();
+void print_Length();
+void print_Nodes();
 
-void printnode();
+int cal_PathLength(int row);
+int cal_InitalPathLength();
+double cal_Distance(int node1, int node2);
+void IntitalPathNearsetNeighbor();
+void RandonTwoNodeSwap();
+int get_RandonNum();
 
-void ReadFile1();
-
-int PathLength(int row);
-int PathLength_row4();
-
-double Caldistance(int node1,int node2);
-
-void Intital_Path();
-void Nearest_Neighbor();
-
+int counttableusestate = 0;
 int main(){
+    reafFile();
     time_t start,finish;
-    int count = 0;
-    ReadFile1();
-
     start = clock();
-    double T = 100;// 初始温度
-    double T_END = 0.01;
-    double Q = 0.99;  // 退火系数
-    int L = 100;//單次溫度迭代
-    double R = 0.9;//接受率
+    double T = 110;// 初始温度
+    double T_END = 0.1;
+    double Q = 0.9996;  // 退火系数
 
-    Intital_Path();
+
+    srand((unsigned)time(NULL));
+    IntitalPathNearsetNeighbor();
     int f1,f2,df;
-    while(T > T_END) {
-        for (int i = 0; i < L; i++) {
+    int count = 0;
+
+
+    while (T > T_END) {
+        for (int j = 0; j < totalNodes; j++) {
+            nodes[j][3] = nodes[j][0];
+        }
+        RandonTwoNodeSwap();
+
+        f1 = cal_PathLength(3);//nodes[][3] old
+        f2 = cal_PathLength(0);//nodes[][0] new
+        df = f2 - f1;
+        if (df < 0) {
+            T = T * Q;
             for (int j = 0; j < totalNodes; j++) {
                 nodes[j][3] = nodes[j][0];
             }
-            Nearest_Neighbor();
-            f1 = PathLength(3);//nodes[][3] old
-            f2 = PathLength(0);//nodes[][0] new
-            df = f2 - f1;
-            if (df < 0) {
+        } else {
+            if (exp((double)-df / T) >= ((double)rand()/(RAND_MAX))) {
+                //printf("%lf\n",exp((double)-df / T));
+                //printf(" rr %lf\n", ((double)rand()/(RAND_MAX + 1.0)));
+                T = T * Q;
                 for (int j = 0; j < totalNodes; j++) {
                     nodes[j][3] = nodes[j][0];
                 }
             } else {
-                if (exp(-df / T) >= R) {
-                    for (int j = 0; j < totalNodes; j++) {
-                        nodes[j][3] = nodes[j][0];
-                    }
-                } else {
-                    for (int j = 0; j < totalNodes; j++) {
-                        nodes[j][0] = nodes[j][3];
-                    }
+                for (int j = 0; j < totalNodes; j++) {
+                    nodes[j][0] = nodes[j][3];
                 }
             }
         }
-        T *= Q;
+  /*      if(counttableusestate == totalNodes*totalNodes-1){
+            break;
+        }*/
+        int NewPathLength = cal_PathLength(3);
+        printf("count : %d\tpath :%d\t %f\n",count,NewPathLength,T);
+
         count++;
+        //print_Nodes();
+        //printf("%d new path :\t",count);
+        //print_Length();
     }
     finish = clock(); // 退火过程结束
+    for(int i = 0 ;i < totalNodes ;i++){
+        printf("%d--->",nodes[i][3]);
+    }
+
+    printf("\ncount:%d\n",count);
     double duration = ((double)(finish-start))/CLOCKS_PER_SEC; // 计算时间
-    int len = PathLength(3);
-    printf("count:%ld\n",count);
-    printf("path length:%ld\n",len);
     printf("time :%lfs.\n",duration);
+    //printf("counttableusestate:%d\n",counttableusestate);
     return 0;
 }
-void Intital_Path(){
-    srand((unsigned)time(NULL));
-    //選任一點為起始節點 i= 0
-    double r1 = ((double)rand()) / (RAND_MAX + 1.0);
-    int index  = (int)(totalNodes * r1);
-
-    int temp = nodes[0][0];
-    nodes[0][0] = nodes[index][0];
-    nodes[index][0] = temp;
-
-    // i 從 1 開始 ，產生 0 ~ 49
-    for (int i=1; i<totalNodes; i++) {
-        nodes[i][0] = rand() % totalNodes;
-        for (int j = 0; j < i; j++) {
-            if (nodes[i][0] == nodes[j][0]) {
-                i--;
-                break;
-            }
-        }
-    }
-    //printnode();
-    int Intital_Path = PathLength(0);
-    printf("Intital_Path : %d\n",Intital_Path);
-}
-
-void Nearest_Neighbor() {
+void IntitalPathNearsetNeighbor(){
     //選任一點為起始節點
-    double r1 = ((double)rand()) / (RAND_MAX + 1.0);
-    int index  = (int)(totalNodes * r1);
+    int index  = get_RandonNum();
     //放入新路徑
-    nodes[0][3] = index;
-    //找到距離最新加入路徑最近的點，將其加入路徑
+    nodes[0][0] = index;
+    //找到距離最近加入路徑最近的點，將其加入路徑
     double distance = 1000;
     for(int i = 0 ;i < 50 ; i++) {
         double j_mindistance = 1000;
         for (int j = 0; j < totalNodes; j++) {
-            if (j == index || j == i ||nodes[i][3] == j)
-                distance = 1000;
-            else
-                distance = Caldistance(nodes[i][3], j);
+            if (j == index || j == i || j == nodes[i][0]) //j == index : 取一次取點取到原點
+                distance = 1000;                         //j == i : 取道同一點
+            else                                          //j == nodes[i][0]判斷是否為以取過的職
+                distance = cal_Distance(nodes[i][0], j);
             if (distance < j_mindistance && nodes[j][5] == 0) {
                 j_mindistance = distance;
-                nodes[i+1][3] = j;
+                nodes[i+1][0] = j;//紀錄是哪個點距離nodes[i][0]最近
                 distance += 0.5;
-                nodes[i+1][4] = (int)distance;
+                nodes[i+1][4] = (int)distance;//記下初始路徑依序兩點距離，方便計算總初始距離
             }
-        } ㄘㄛ
-        nodes[nodes[i+1][3]][5] = 1;
+        }
+        nodes[nodes[i+1][0]][5] = 1;//作為標籤避免重複找
     }
-    for(int i = 0; i < totalNodes ;i++){
+    for(int i = 0; i < totalNodes ;i++){//計算最後一個點與遠點的距離
         if(nodes[i][5]==0){
-            nodes[0][4] = (int)Caldistance(nodes[totalNodes-1][3], nodes[0][3]);
+            nodes[0][4] = (int) cal_Distance(nodes[i][0], nodes[0][0]);
             nodes[i][5] = 1 ;
         }
     }
-    //printnode();
-    int Nearest_Neighbor = PathLength(3);
-    int Nearest_Neighbor1 = PathLength_row4();
-    printf("Nearest_Neighbor : %d\t%d\n",Nearest_Neighbor,Nearest_Neighbor1);
+    //print_Nodes();
+    printf("inital path : \t");
+    //print_Length();
 }
 
-double Caldistance(int node1,int node2)
-{
+int cal_InitalPathLength() {
+    int path = 0;
+    for(int i= 0 ;i < totalNodes; i++)
+        path += nodes[i][4];
+    return path;
+}
+
+void RandonTwoNodeSwap(){
+    int index1 = get_RandonNum();
+    int index2 = get_RandonNum();
+    //printf("%d\t%d\n",index1,index2);
+    //if(table[index1][index2] == 0){
+        int temp = nodes[index1][0];
+        nodes[index1][0] = nodes[index2][0];
+        nodes[index2][0] = temp;
+        //table[index1][index2] = 1;//使用過記錄到table
+      //  counttableusestate++;
+    //}
+}
+
+int get_RandonNum() {
+    double R = ((double)rand()) / (RAND_MAX + 1.0);
+    int RandonNum  = (int)(totalNodes * R);
+    //printf("%d\n",RandonNum);
+    return RandonNum;
+}
+
+double cal_Distance(int node1, int node2) {
     int x1 = nodes[node1][1];
     int y1 = nodes[node1][2];
     int x2 = nodes[node2][1];
     int y2 = nodes[node2][2];
     int x = (x1-x2)*(x1-x2);
     int y = (y1-y2)*(y1-y2);
-    double dis = sqrt(x+y);
-    //printf("--%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\t\n",node1,node2,x1,y1,x2,y2,x,y,dis);
-    return dis;
+    return sqrt(x+y);
 }
 
-int PathLength_row4()
-{
+int cal_PathLength(int row) {
     int path = 0;
-    for(int i= 0 ;i < totalNodes; i++)
-        path += nodes[i][4];
-    return path;
-}
-int PathLength(int row)
-{
-    double path = 0;
     for(int i= 0 ;i < totalNodes-1; i++){
         int node1 = nodes[i][row];
         int node2 = nodes[i+1][row];
-        double dis = Caldistance(node1,node2) + 0.5;
+        double dis = cal_Distance(node1, node2) + 0.5;
         path += (int)dis;
     }
-    path += Caldistance(nodes[totalNodes-1][row],nodes[0][row]);
+    path += cal_Distance(nodes[totalNodes - 1][row], nodes[0][row]);
     return path;
 }
 
-void printnode(){
+void print_Nodes(){
     for(int i=0;i<50;i++)
         printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",i,nodes[i][0],nodes[i][1],nodes[i][2],nodes[i][3],nodes[i][4],nodes[i][5]);
 }
-
-void ReadFile1() {
+void print_Length() {
+    int NewPathLength = cal_PathLength(3);
+    int InitalPathLength = cal_InitalPathLength();
+    printf("%d\t%d\n",InitalPathLength,NewPathLength);
+}
+void reafFile() {
     fstream file;
     file.open("eil51.txt", ios_base::in);
     char line[totalNodes];
@@ -182,8 +191,7 @@ void ReadFile1() {
     while(file.getline(line, sizeof(line),'\n')){
         p = strtok(line,d);
         int j=0;
-        while(p)
-        {
+        while(p) {
             stringstream strValue;
             strValue << p;
             unsigned int temp;
@@ -194,6 +202,6 @@ void ReadFile1() {
         }
         i++;
     }
-    //printnode();
+    print_Nodes();
     cout << "Read file done" << endl;
 }
