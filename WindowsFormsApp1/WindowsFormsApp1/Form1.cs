9909f5ee4988411b7d466f7f634b5a5e93d33b05
James992927108 <james992927108@gmail.com>
2017-12-19 00:39:19 +0800

    實驗結果

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -448,6 +448,7 @@ namespace WindowsFormsApp1
                 if (step == (int)Form1.step.Eliminate)
                 {
                     this.panel1.Refresh();
+                    DrawNode(NodeNumList[CurrentDataIndex]);//根據點數量，先畫出點
                     EdgeList.Clear();
                     OutputNodeList.Clear();
                     for (int i = 0; i < HP.Count - 1; i++)
